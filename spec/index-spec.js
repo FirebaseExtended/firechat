338cdfb1ad7aa60d64fe36a4581d06477a76d11d
Mark Nyon <mark@grandkru.com>
2015-06-15 08:11:15 -0400

    Updated  version

--- spec/index-spec.js
+++
@@ -1,8 +0,0 @@
-describe('Loaded Libraries', function() {
-  it('The Firebase library should be loaded', function() {
-  	var createFirebase = function() {
-  		return new Firebase('https://brilliant-fire-2797.firebaseio.com');
-  	};
-  	expect(createFirebase).not.toThrow();
-  });
-});
