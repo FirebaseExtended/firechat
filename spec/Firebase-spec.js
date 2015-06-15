8f6ce44459e6af5b4f397c71129d44ebe1f186fe
Mark Nyon <mark@grandkru.com>
2015-06-15 09:17:01 -0400

    Removed bad spec

--- spec/Firebase-spec.js
+++ spec/Firebase-spec.js
@@ -6,16 +6,3 @@ describe('Loaded Libraries', function() {
   	expect(createFirebase).not.toThrow();
   });
 });
-
-describe("Firebase UserRef", function() {
-	it('should have a user Reference', function (done) {
-		loadFixtures('fixtures/index.html');
-		var createFirebase = function() {
-  		return new Firebase('https://brilliant-fire-2797.firebaseio.com');
-  	};
-    var tokenGenerator = new FirebaseTokenGenerator("LENnUH4jYdZSk4864WZDk5VgojFCE1h7UTKXcBW3");
-    var chatToken = tokenGenerator.createToken({uid: "custom:1", name: "Mark Nyon"});
-
-  	var firebaseRef = createFirebase();
-	});
-});
